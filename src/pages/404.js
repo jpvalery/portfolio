e3118537554b20cff0c76941e68af07bf070639a
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-02-26 22:00:47 -0800

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import Img from "gatsby-image"
+import styled from 'styled-components';
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+import * as palette from '../utils/styles';
+
+const Wrapper = styled.div`
+`
+const Grid = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: repeat( 1fr);
+  grid-auto-flow: row dense;
+  grid-gap: 1rem;
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
+const GridItem = styled.div`
+height: ${palette.HEIGHT };
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+text-align: center;
+`
+
+const NotFoundPage = () => {
+return (
+  <Wrapper>
+    <GridItem>
+      <h1>404 NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </GridItem>
+  </Wrapper>
 )
-
+}
 export default NotFoundPage

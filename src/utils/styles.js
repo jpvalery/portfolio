c064f377e03f5c255976101fb71dcfd3a12cf845
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-03-06 14:56:52 -0800

    Update styles.js

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -12,7 +12,7 @@ export const CONTENT_HEIGHT = '90vh';
 
 export const HEIGHT = '80vh';
 
-export const BLURB_HEIGHT = '75vh';
+export const BLURB_HEIGHT = '61.8vh';
 
 export const BLURB_HEIGHT_MOBILE = '90vh';
 

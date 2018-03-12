9c57ab8d9968d8bd31af469e91299c957d4177da
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-03-11 21:02:12 -0700

    set up favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,24 @@ module.exports = {
       respectDNT: true,
     },
   },
+  {
+    resolve: `gatsby-plugin-favicon`,
+    options: {
+      logo: "./favicon.png",
+      injectHTML: true,
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    }
+  }
 
     ],
 };

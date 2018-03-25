09de431246715494a2f2727a443e2402582668f0
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-03-25 11:10:05 -0700

    Adjust social icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -17,14 +17,8 @@ const Footer = () => {
       height: 1.618rem;
       margin: 0;
     }
+  `;
 
-  `;
-  const Social = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-rows: 1fr;
-    grid-gap: 1rem;
-  `;
   const Grid = styled.div`
     width: 100%;
     display: grid;
@@ -32,6 +26,16 @@ const Footer = () => {
     grid-gap: 1rem;
     @media (max-width: 991px) {
       grid-template-columns: 1fr;
+      grid-gap: 0.5rem;
+    }
+  `;
+  const Social = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-gap: 1rem;
+    @media (max-width: 991px) {
+      grid-gap: 0.5rem;
     }
   `;
   const Content = styled.p`
@@ -45,7 +49,7 @@ const Footer = () => {
       font-family: Oswald;
     }
     @media (max-width: 991px) {
-    text-align: center;
+      text-align: center;
     }
   `;
   const Icon = styled.div`
@@ -60,6 +64,9 @@ const Footer = () => {
     }
     @media (max-width: 991px) {
       justify-content: center;
+      img {
+        margin: 0.25rem 1rem;
+      }
     }
   `;
   const SocialIcon = styled.div`
@@ -74,47 +81,53 @@ const Footer = () => {
     }
     @media (max-width: 991px) {
       justify-content: center;
+      img {
+        margin: 0.25rem 1rem;
+      }
     }
   `;
 
   return (
     <Wrapper>
-    <Grid>
-      <Content>
-        &copy; {new Date().getFullYear()} Matthias Kronfeld Jordan
-        <br />Photographer & Content Creator
-        <br />Product & Marketing Coordinator @ Surf Air
-        <br />Made with ♡ in Santa Monica, California, USA.
-        <br /><br /><a href="/resume"><span>RESUME 2018</span></a>
-      </Content>
-      <Social>
-      <SocialIcon>
-      <a href="https://instagram.com/iammatthias/">
-        <img src={withPrefix("./instagram.svg")} />
-      </a>
-      <a href="https://twitter.com/iamMatthias">
-        <img src={withPrefix("./twitter.svg")} />
-      </a>
-      <a href="https://www.linkedin.com/in/iammatthias/">
-        <img src={withPrefix("./linkedin.svg")} />
-      </a>
-      <a href="https://www.facebook.com/iammatthias">
-        <img src={withPrefix("./facebook.svg")} />
-      </a>
-
-      </SocialIcon>
-      <Icon>
-        <a href="https://www.contentful.com/" rel="nofollow">
-          <img src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" />
-        </a>
-        <a href="https://www.gatsbyjs.org" rel="nofollow">
-          <img src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667" />
-        </a>
-        <a href="https://www.netlify.com" rel="nofollow">
-          <img src="https://cdn.netlify.com/1e66438712c70d3dfdcfe11f31e467864f94b803/b2c28/img/press/logos/full-logo-light.svg" />
-        </a>
-      </Icon>
-      </Social>
+      <Grid>
+        <Content>
+          &copy; {new Date().getFullYear()} Matthias Kronfeld Jordan
+          <br />Photographer & Content Creator
+          <br />Product & Marketing Coordinator @ Surf Air
+          <br />Made with ♡ in Santa Monica, California, USA.
+          <br />
+          <br />
+          <a href="/resume">
+            <span>RESUME 2018</span>
+          </a>
+        </Content>
+        <Social>
+          <SocialIcon>
+            <a href="https://instagram.com/iammatthias/">
+              <img src={withPrefix("./instagram.svg")} />
+            </a>
+            <a href="https://twitter.com/iamMatthias">
+              <img src={withPrefix("./twitter.svg")} />
+            </a>
+            <a href="https://www.linkedin.com/in/iammatthias/">
+              <img src={withPrefix("./linkedin.svg")} />
+            </a>
+            <a href="https://www.facebook.com/iammatthias">
+              <img src={withPrefix("./facebook.svg")} />
+            </a>
+          </SocialIcon>
+          <Icon>
+            <a href="https://www.contentful.com/" rel="nofollow">
+              <img src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" />
+            </a>
+            <a href="https://www.gatsbyjs.org" rel="nofollow">
+              <img src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667" />
+            </a>
+            <a href="https://www.netlify.com" rel="nofollow">
+              <img src="https://cdn.netlify.com/1e66438712c70d3dfdcfe11f31e467864f94b803/b2c28/img/press/logos/full-logo-light.svg" />
+            </a>
+          </Icon>
+        </Social>
       </Grid>
     </Wrapper>
   );

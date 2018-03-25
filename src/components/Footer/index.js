2531cf1c88ea1e6a15f69c9fb27c60fc15fec884
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-03-25 10:57:25 -0700

    Upload

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { withPrefix } from "gatsby-link";
+import Img from "gatsby-image";
 import styled from "styled-components";
 
 import * as palette from "../../utils/styles";
@@ -15,32 +17,69 @@ const Footer = () => {
       height: 1.618rem;
       margin: 0;
     }
+
+  `;
+  const Social = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-gap: 1rem;
+  `;
+  const Grid = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1rem;
+    @media (max-width: 991px) {
+      grid-template-columns: 1fr;
+    }
   `;
   const Content = styled.p`
     color: ${palette.SECONDARY_COLOR};
     padding: 1rem;
-    text-align: center;
+    text-align: left;
     font-size: 10px;
     margin: 0;
     span {
       font-weight: 900;
       font-family: Oswald;
     }
+    @media (max-width: 991px) {
+    text-align: center;
+    }
   `;
   const Icon = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-end;
     position: relative;
     img {
       height: 1.618rem;
       margin: 1rem;
     }
+    @media (max-width: 991px) {
+      justify-content: center;
+    }
+  `;
+  const SocialIcon = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    position: relative;
+    img {
+      height: 1.382rem;
+      margin: 1rem;
+    }
+    @media (max-width: 991px) {
+      justify-content: center;
+    }
   `;
 
   return (
     <Wrapper>
+    <Grid>
       <Content>
         &copy; {new Date().getFullYear()} Matthias Kronfeld Jordan
         <br />Photographer & Content Creator
@@ -48,6 +87,22 @@ const Footer = () => {
         <br />Made with ♡ in Santa Monica, California, USA.
         <br /><br /><a href="/resume"><span>RESUME 2018</span></a>
       </Content>
+      <Social>
+      <SocialIcon>
+      <a href="https://instagram.com/iammatthias/">
+        <img src={withPrefix("./instagram.svg")} />
+      </a>
+      <a href="https://twitter.com/iamMatthias">
+        <img src={withPrefix("./twitter.svg")} />
+      </a>
+      <a href="https://www.linkedin.com/in/iammatthias/">
+        <img src={withPrefix("./linkedin.svg")} />
+      </a>
+      <a href="https://www.facebook.com/iammatthias">
+        <img src={withPrefix("./facebook.svg")} />
+      </a>
+
+      </SocialIcon>
       <Icon>
         <a href="https://www.contentful.com/" rel="nofollow">
           <img src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" />
@@ -59,6 +114,8 @@ const Footer = () => {
           <img src="https://cdn.netlify.com/1e66438712c70d3dfdcfe11f31e467864f94b803/b2c28/img/press/logos/full-logo-light.svg" />
         </a>
       </Icon>
+      </Social>
+      </Grid>
     </Wrapper>
   );
 };

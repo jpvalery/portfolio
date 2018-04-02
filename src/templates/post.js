d81e1979e0d4bfeb42ce6860c91c76202801656e
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-04-01 17:11:36 -0700

    Add Prism.js to blog functionality

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import Img from "gatsby-image";
 import styled from "styled-components";
 
+require("../utils/prism.css");
+
 import * as palette from "../utils/styles";
 
 class Blog extends Component {

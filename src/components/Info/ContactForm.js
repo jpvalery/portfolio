5200d682320e876c3c11cca2a787be7335d2b673
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-09-07 14:15:43 -0700

    Lifted form config from CMD

--- src/components/Info/ContactForm.js
+++ src/components/Info/ContactForm.js
@@ -174,7 +174,7 @@ class ContactForm extends React.Component {
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'Contact', ...this.state }),
+      body: encode({ 'form-name': 'contact', ...this.state }),
     })
       .then(this.handleSuccess)
       .catch(error => alert(error))
@@ -197,22 +197,23 @@ class ContactForm extends React.Component {
   render() {
     return (
       <Form
-        name="Contact"
+        name="contact"
         onSubmit={this.handleSubmit}
         data-netlify="true"
         data-netlify-honeypot="bot"
         overlay={this.state.showModal}
         onClick={this.closeModal}
       >
-        <input type="hidden" name="form-name" value="Contact" />
+        <input type="hidden" name="form-name" value="contact" />
         <p hidden>
           <label>
             Don’t fill this out:{' '}
             <input name="bot" onChange={this.handleInputChange} />
           </label>
         </p>
-
+        <label for="name">Name</label>
         <Name
+          id="name"
           name="name"
           type="text"
           placeholder="Full Name"
@@ -220,7 +221,9 @@ class ContactForm extends React.Component {
           onChange={this.handleInputChange}
           required
         />
+        <label for="email">Email</label>
         <Email
+          id="email"
           name="email"
           type="email"
           placeholder="Email"
@@ -228,7 +231,9 @@ class ContactForm extends React.Component {
           onChange={this.handleInputChange}
           required
         />
+        <label for="message">Message</label>
         <Message
+          id="message"
           name="message"
           type="text"
           placeholder="Message"
@@ -236,22 +241,26 @@ class ContactForm extends React.Component {
           onChange={this.handleInputChange}
           required
         />
+
         <Submit name="submit" type="submit" value="Send" />
 
         <Modal visible={this.state.showModal}>
-          <p>
+          <h2>
             Thank you for reaching out. I will get back to you as soon as
             possible.
-          </p>
-          <Button onClick={this.closeModal}>Okay</Button>
+          </h2>
+          <Submit
+            name="okay"
+            type="submit"
+            onClick={this.closeModal}
+            value="Okay"
+          />
         </Modal>
       </Form>
     )
   }
 }
-
 ContactForm.propTypes = {
   data: PropTypes.object,
 }
-
 export default ContactForm

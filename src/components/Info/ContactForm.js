0a911a25b6e8792228fc57dd6cdf6e3c9172cc5c
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-09-17 12:19:34 -0700

    Testing if `/contact?no-cache=1` is still needed after package update

--- src/components/Info/ContactForm.js
+++ src/components/Info/ContactForm.js
@@ -171,7 +171,7 @@ class ContactForm extends React.Component {
   }
 
   handleSubmit = event => {
-    fetch('/contact?no-cache=1', {
+    fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({ 'form-name': 'contact', ...this.state }),

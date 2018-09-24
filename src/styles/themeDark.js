9794d44ea095608b1e1b3d2a2f1a520a47ab4d8e
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-09-24 13:58:04 -0700

    Color tweaks

--- src/styles/themeDark.js
+++ src/styles/themeDark.js
@@ -2,7 +2,7 @@ const theme = {
   colors: {
     base: '#100B00', // White Smoke — Site background
     secondary: '#E6E3E1', // JAPANESE INDIGO — Default text color
-    tertiary: '#f0e3e1', // SMOKY BLACK — Bold text color, header color, etc
+    tertiary: '#F4DAD3', // SMOKY BLACK — Bold text color, header color, etc
     highlight: '#FE5F55', // CARMINE PINK — Accent, use for underlines, highlights, etc
     white: '#FCFCFC', // White
   },
@@ -18,9 +18,9 @@ const theme = {
 }
 
 export const BASE = '#100B00'
-export const SECONDARY = '#FBF2F0'
+export const SECONDARY = '#E6E3E1'
 export const TERTIARY = '#F4DAD3'
 export const HIGHLIGHT = '#FE5F55'
-export const WHITE = '#223843'
+export const WHITE = '#FCFCFC'
 
 export default theme

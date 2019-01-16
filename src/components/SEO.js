e766d57d1033af7480e78194fe08eb609ca14369
jpvalery <jp.valery@me.com>
2019-01-15 20:17:10 -0500

    Initial setup (.contenful.json) Editing a couple of things I could do.
    Getting this error when trying to do gatsby develop:
    
    error Cannot read property 'allContentfulPost' of undefined
    
      TypeError: Cannot read property 'allContentfulPost' of undefined
    
      - gatsby-node.js:18 graphql.then.result
        /Users/jpvalery/repos/portfolio-matthiasnet/gatsby-node.js:18:19
    
      - util.js:16 tryCatcher
        [lib]/[gatsby-cli]/[bluebird]/js/release/util.js:16:23
    
      - promise.js:512 Promise._settlePromiseFromHandler
        [lib]/[gatsby-cli]/[bluebird]/js/release/promise.js:512:31
    
      - promise.js:569 Promise._settlePromise
        [lib]/[gatsby-cli]/[bluebird]/js/release/promise.js:569:18
    
      - promise.js:606 Promise._settlePromiseCtx
        [lib]/[gatsby-cli]/[bluebird]/js/release/promise.js:606:10
    
      - async.js:142 _drainQueueStep
        [lib]/[gatsby-cli]/[bluebird]/js/release/async.js:142:12
    
      - async.js:131 _drainQueue
        [lib]/[gatsby-cli]/[bluebird]/js/release/async.js:131:9
    
      - async.js:147 Async._drainQueues
        [lib]/[gatsby-cli]/[bluebird]/js/release/async.js:147:5
    
      - async.js:17 Immediate.Async.drainQueues [as _onImmediate]
        [lib]/[gatsby-cli]/[bluebird]/js/release/async.js:17:14
    
    error UNHANDLED REJECTION
    
      TypeError: Cannot read property 'allContentfulPost' of undefined
    
      - gatsby-node.js:18 graphql.then.result
        /Users/jpvalery/repos/portfolio-matthiasnet/gatsby-node.js:18:19
    
      - util.js:16 tryCatcher
        [lib]/[gatsby-cli]/[bluebird]/js/release/util.js:16:23
    
      - promise.js:512 Promise._settlePromiseFromHandler
        [lib]/[gatsby-cli]/[bluebird]/js/release/promise.js:512:31
    
      - promise.js:569 Promise._settlePromise
        [lib]/[gatsby-cli]/[bluebird]/js/release/promise.js:569:18
    
      - promise.js:606 Promise._settlePromiseCtx
        [lib]/[gatsby-cli]/[bluebird]/js/release/promise.js:606:10
    
      - async.js:142 _drainQueueStep
        [lib]/[gatsby-cli]/[bluebird]/js/release/async.js:142:12
    
      - async.js:131 _drainQueue
        [lib]/[gatsby-cli]/[bluebird]/js/release/async.js:131:9
    
      - async.js:147 Async._drainQueues
        [lib]/[gatsby-cli]/[bluebird]/js/release/async.js:147:5
    
      - async.js:17 Immediate.Async.drainQueues [as _onImmediate]
        [lib]/[gatsby-cli]/[bluebird]/js/release/async.js:17:14

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -34,7 +34,7 @@ class SEO extends Component {
 
     // Replace with Page Parameters if post or page
     if (postSEO || pageSEO || gallerySEO) {
-      title = 'I AM MATTHIAS' + ' ' + '-' + ' ' + postNode.title
+      title = 'Jp Valery' + ' ' + '-' + ' ' + postNode.title
       description = postNode.body.childMarkdownRemark.excerpt
 
       pageUrl = config.siteUrl + '/' + pagePath + '/'

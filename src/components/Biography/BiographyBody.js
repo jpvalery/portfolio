29b600bcedd6793362eabd97d84e3ea7ee0c5d2e
jpvalery <jp.valery@me.com>
2019-02-10 18:01:02 -0500

    Adding Fadein/Fadeup on Contact/Bio/Blog/Home
    Fadein : progressive fade from opacity 0 to 1
    Fadeup : fadein + 2rem translation up

--- src/components/Biography/BiographyBody.js
+++ src/components/Biography/BiographyBody.js
@@ -11,6 +11,19 @@ const Wrapper = styled.section`
   grid-template-rows: repeat(auto);
   grid-gap: 0;
   grid-template-areas: 'BiographyBodyTop' 'BiographyBodyBottom';
+  -webkit-animation: fadeup 2.5s; /* Safari, Chrome and Opera > 12.1 */
+     -moz-animation: fadeup 2.5s; /* Firefox < 16 */
+      -ms-animation: fadeup 2.5s; /* Internet Explorer */
+       -o-animation: fadeup 2.5s; /* Opera < 12.1 */
+          animation: fadeup 2.5s;
+
+@keyframes fadeup { from { opacity: 0; transform: translateY(2rem);}  to  { opacity: 1; transform: translateY(0) }}
+/* Firefox < 16 */
+@-moz-keyframes fadeup { from { opacity: 0; transform: translateY(2rem);}  to  { opacity: 1; transform: translateY(0) }}
+/* Safari, Chrome and Opera > 12.1 */
+@-webkit-keyframes fadeup { from { opacity: 0; transform: translateY(2rem);}  to  { opacity: 1; transform: translateY(0) }}
+/* Internet Explorer */
+@-ms-keyframes fadeup { from { opacity: 0; transform: translateY(2rem);}  to  { opacity: 1; transform: translateY(0) }}
 `
 
 const BiographyBody = props => {
--- src/components/Home/HomeBodyTop.js
+++ src/components/Home/HomeBodyTop.js
@@ -8,6 +8,21 @@ const Wrapper = styled.section`
   padding: 2rem;
   display: grid;
   grid-template-rows: repeat(auto);
+
+    -webkit-animation: fadein 2.5s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein 2.5s; /* Firefox < 16 */
+        -ms-animation: fadein 2.5s; /* Internet Explorer */
+         -o-animation: fadein 2.5s; /* Opera < 12.1 */
+            animation: fadein 2.5s;
+
+@keyframes fadein { from { opacity: 0; }  to  { opacity: 1; }}
+/* Firefox < 16 */
+@-moz-keyframes fadein { from { opacity: 0; }  to  { opacity: 1; }}
+/* Safari, Chrome and Opera > 12.1 */
+@-webkit-keyframes fadein { from { opacity: 0; }  to  { opacity: 1; }}
+/* Internet Explorer */
+@-ms-keyframes fadein { from { opacity: 0; }  to  { opacity: 1; }}
+
 `
 const Body = styled.div`
   margin: 0;

8d09455b3003621b96157e16f76aeddeddc75d9a
jpvalery <jpvalery@users.noreply.github.com>
2021-03-01 15:16:58 -0500

    feat: graphql query now works!!! ffs

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import NextLink from 'next/link'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-export default function Home() {
+export default function Home({ galleries }) {
+  console.log(galleries);
   return (
     <div className="mx-auto py-24 grid gap-20 md:gap-30">
       <div className="pb-8">
@@ -17,6 +19,62 @@ export default function Home() {
           </p>
         </div>
       </div>
+      <div className="grid">
+        {galleries.map(gallery => {
+          return (
+            <p>{gallery.title}</p>
+          );
+        })}
+        </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+
+  const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
+  const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+
+  const client = new ApolloClient({
+  uri: `https://graphql.contentful.com/content/v1/spaces/${space}/?access_token=${accessToken}`,
+  cache: new InMemoryCache(),
+  credentials: 'same-origin',
+  });
+
+
+  const { data } = await client.query({
+  query: gql`
+  query Index {
+    extendedGalleryCollection(order: publishDate_DESC, where: {displayHome: true}) {
+      items {
+        title
+        slug
+        year
+        body
+        metaDescription
+        summary
+        publishDate
+        displayHome
+        heroImage {
+          title
+          description
+          contentType
+          fileName
+          size
+          url
+          width
+          height
+        }
+      }
+    }
+  }
+  `
+});
+
+
+  return {
+    props: {
+      galleries: data.extendedGalleryCollection.items
+    }
+  }
 }

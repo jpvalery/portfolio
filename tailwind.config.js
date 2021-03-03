699f0b0db7838008d2e71f5809dbe97545fbaa08
jpvalery <jpvalery@users.noreply.github.com>
2021-03-03 15:17:57 -0500

    chore: fixes + prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      'md': '720px',
+      md: '720px',
       ...defaultTheme.screens,
     },
     extend: {
@@ -32,31 +32,43 @@ module.exports = {
                 color: '#D8D8C7',
               },
             },
+            strong: {
+              color: '#D8D8C7',
+            },
           },
         },
       },
       scale: {
-        '115': '1.15',
+        115: '1.15',
       },
       rotate: {
-        '4': '4deg',
+        4: '4deg',
         '-4': '-4deg',
       },
       spacing: {
-         'max2': '26rem',
-         'max3': '40rem',
-
-       },
+        max2: '26rem',
+        max3: '40rem',
+      },
     },
   },
   variants: {
-    extend: { transform: ['hover', 'focus', 'group-hover',], rotate: ['odd', 'even', 'group-hover', '3n',], translate: ['odd', 'even', 'group-hover', '3n',], scale: ['hover', 'focus', 'group-hover',], inset: ['first', 'even', 'odd', '3n',],},
+    extend: {
+      transform: ['hover', 'focus', 'group-hover'],
+      rotate: ['odd', 'even', 'group-hover', '3n'],
+      translate: ['odd', 'even', 'group-hover', '3n'],
+      scale: ['hover', 'focus', 'group-hover'],
+      inset: ['first', 'even', 'odd', '3n'],
+    },
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio'),plugin(function({ addVariant, e }) {
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    plugin(function ({ addVariant, e }) {
       addVariant('3n', ({ modifySelectors, separator }) => {
         modifySelectors(({ className }) => {
           return `.${e(`3n${separator}${className}`)}:nth-child(3n+0)`
         })
       })
-    })],
+    }),
+  ],
 }

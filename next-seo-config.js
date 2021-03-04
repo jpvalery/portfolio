18a160977f0791c66891e314863f3258e8b9b5f9
jpvalery <jpvalery@users.noreply.github.com>
2021-03-04 08:56:43 -0500

    fix: SEO

--- next-seo-config.js
+++ next-seo-config.js
@@ -1,29 +1,32 @@
 const title = 'Jp Valery - Photographer documenting spaces and people'
 const description =
   'Selected shots from a photographer relentlessly documenting and exploring spaces and people.'
+const siteUrl = "https://jpvalery.photo"
 
 const SEO = {
-  title,
-  description,
-  canonical: 'https://jpvalery.photo',
+  title: title,
+  description: description,
+  canonical: siteUrl,
   openGraph: {
-    type: 'website',
-    locale: 'en_CA',
-    url: 'https://jpvalery.photo',
-    title,
-    description,
+    type: "website",
+    locale: "en_CA",
+    url: siteUrl,
+    title: title,
+    description: description,
     images: [
       {
-        url: 'https://jpvalery.photo/static/images/og-banner.jpg',
-        alt: title,
+        url: 'https://jpvalery.photo/static/og-banner.jpg',
+        width: 1920,
+        height: 1280,
+        alt: 'Open Graph Image',
       },
     ],
   },
   twitter: {
-    handle: '@jpvalery',
-    site: 'jpvalery.photo',
-    cardType: 'summary_large_image',
+    handle: "@jpvalery",
+    site: siteUrl,
+    cardType: "summary_large_image",
   },
-}
+};
 
-export default SEO
+export default SEO;

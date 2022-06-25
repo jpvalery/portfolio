cd1736d32f0d5ad85b28236d7f349868a93409b6
@jpvalery <jpvalery@users.noreply.github.com>
2022-06-25 18:11:45 -0400

    fix: wrong icon

--- components/Header.js
+++ components/Header.js
@@ -168,7 +168,15 @@ export default function Header() {
                                   <p className="inline-flex text-base font-medium text-white">
                                     Unsplash
                                     <div className="mt-1 ml-2 h-4 w-4 text-gray-500">
-                                      <Unsplash />
+                                      <svg
+                                        className="mt-1 ml-2 h-4 w-4 text-gray-500"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                      >
+                                        <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                                        <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                                      </svg>
                                     </div>
                                   </p>
                                   <p className="mt-1 text-sm text-gray-200">
